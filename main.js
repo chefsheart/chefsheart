b08accb35332f0c2ec09a769201f8868e367f3cc
chefsheart <thechefsheart@gmail.com>
2025-08-27 21:22:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//mobile menu
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.querySelector('.hamburger-button');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
+});
+
+
+// counter
+  const counter = document.querySelector('.count');
+  const target = +counter.getAttribute('data-target');
+  const speed = 150; // lower is faster
+
+  const updateCount = () => {
+    const current = +counter.innerText;
+    const increment = Math.ceil(target / speed);
+
+    if (current < target) {
+      counter.innerText = current + increment;
+      setTimeout(updateCount, 10);
+    } else {
+      counter.innerText = target; // prevent overshoot
+    }
+  };
+
+  window.addEventListener('load', updateCount);
